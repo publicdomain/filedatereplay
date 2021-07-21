745635b001c924fac3a74b4b1a48d0018925ae1c
Victor <publicdomainvic@gmail.com>
2021-07-21 09:15:57 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick() code

--- FileDateReplay/MainForm.cs
+++ FileDateReplay/MainForm.cs
@@ -296,7 +296,8 @@ namespace FileDateReplay
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub repository
+            Process.Start("https://github.com/publicdomain/filedatereplay");
         }
 
         /// <summary>
